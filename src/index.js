b5cb4ad15e0f86f52d800af0d9ae8c5df764f6d4
wi2 <contact@wi2prod.fr>
2015-07-05 00:27:29 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+"use strict";
+
+import React from 'react'
+import {ReactArcSonic} from 'react-sonic'
+
+export class ReactImg extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  onloadImage() {
+    this.state.loaded = true;
+    this.forceUpdate();
+  }
+  getDim() {
+    this.width = this.refs.image.getDOMNode().width||this.props.width;
+    this.height = this.refs.image.getDOMNode().height||this.props.height;
+    this.state.loading = true;
+    this.forceUpdate();
+  }
+  componentDidMount() {
+    this.image = new Image();
+    this.image.onload = this.onloadImage.bind(this);
+    this.image.src = this.props.src;
+    this.image.setAttribute("class", this.props.className);
+    this.getDim();
+  }
+  render() {
+    if (this.state.loaded)
+      return <img src={this.props.src} className={this.props.className||''} alt={this.props.alt||''} />
+    else if (this.state.loading)
+      return <ReactArcSonic fillColor="#FF0000" width={this.width} height={this.height} className={this.props.className||''} title={this.props.alt||''} />
+    else
+      return <img ref="image" className={this.props.className||''} alt={this.props.alt||''} />
+  }
+}
---
+++ test.js
@@ -0,0 +1,4 @@
+var assert = require('assert');
+var ReactImg = require('./index.js');
+
+console.log(ReactImg);
